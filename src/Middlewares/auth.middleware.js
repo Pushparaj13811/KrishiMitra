076c19d6afb2bb3362c2b109379ac3d02b9e8e9c
Hompushparaj Mehta <pushparajmehta002@gmail.com>
2024-08-01 09:36:49 +0530

    feat: Add authentication middleware for verifying JWT tokens

---
+++ src/Middlewares/auth.middleware.js
@@ -0,0 +1,32 @@
+import { asyncHandler } from "../Utils/asyncHandler.js";
+import { ApiError } from "../Utils/apiError.js";
+import { User } from "../Models/users.model.js";
+import jwt from "jsonwebtoken";
+
+export const verifyJwt = asyncHandler(async (req, res, next) => {
+  try {
+    const token =
+      req.cookies?.accessToken ||
+      req.header("Authorization").replace("Bearer ", "");
+
+    if (!token) {
+      throw new ApiError(401, "Unauthorized request");
+    }
+
+    const decodedToken = jwt.verify(token, process.env.ACCESS_TOKEN_SECRET);
+
+    const user = await User.findById(decodedToken?._id).select(
+      "-password -refreshToken"
+    );
+
+    if (!user) {
+      throw new ApiError(401, "Unauthorized request");
+    }
+
+    req.user = user;
+
+    next();
+  } catch (error) {
+    throw new ApiError(401, error?.message || "Invalid Access Token");
+  }
+});
