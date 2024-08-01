a74e5566b3103381746a57d6b294f0fed5e588a9
Hompushparaj Mehta <pushparajmehta002@gmail.com>
2024-08-01 09:37:10 +0530

    refactor: Fix typo in apiError.js stack variable name

--- src/Utils/apiError.js
+++ src/Utils/apiError.js
@@ -3,7 +3,7 @@ class ApiError extends Error {
     statuscode,
     message = "Internal Server Error",
     errors = [],
-    stact = ""
+    stack = ""
   ) {
     super(message);
     this.statuscode = statuscode;
