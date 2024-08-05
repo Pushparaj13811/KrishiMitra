9e1e765a983f5b78f212e7372b20b29c7227e769
Hompushparaj Mehta <pushparajmehta002@gmail.com>
2024-08-05 09:51:17 +0530

    refactor: Update video.routes.js to use more descriptive route names

--- src/Routers/video.routes.js
+++ src/Routers/video.routes.js
@@ -1,9 +1,8 @@
 import { Router } from "express";
 import {
-  createVideo,
+  uploadVideo,
   getAllVideos,
   getVideoById,
-  updateVideo,
   deleteVideo,
 } from "../Controllers/video.controller.js";
 import { verifyJwt } from "../Middlewares/auth.middleware.js";
@@ -14,9 +13,9 @@ const router = Router();
 router.route("/get-all-videos").get(getAllVideos);
 router.route("/get-video/:videoId").get(getVideoById);
 router
-  .route("/create-video")
-  .post(verifyJwt, upload.single("video"), createVideo);
-router.route("/update-video/:videoId").patch(verifyJwt, updateVideo);
+  .route("/upload-video")
+  .post(verifyJwt, upload.single("video"), uploadVideo);
+// router.route("/update-video/:videoId").patch(verifyJwt, updateVideo);
 router.route("/delete-video/:videoId").delete(verifyJwt, deleteVideo);
 
 export default router;
