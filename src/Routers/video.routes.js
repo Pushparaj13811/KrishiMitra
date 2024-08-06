622e0168e24e6c7b8095930d6f47999ea6ba2b84
Hompushparaj Mehta <pushparajmehta002@gmail.com>
2024-08-06 09:28:07 +0530

    refactor: Add video crop API endpoints and functions for linking and unlinking videos

--- src/Routers/video.routes.js
+++ src/Routers/video.routes.js
@@ -4,6 +4,8 @@ import {
   getAllVideos,
   getVideoById,
   deleteVideo,
+  uploadVideoToCropVideo,
+  deleteVideoFromCropVideo,
 } from "../Controllers/video.controller.js";
 import { verifyJwt } from "../Middlewares/auth.middleware.js";
 import { upload } from "../Middlewares/multer.middleware.js";
@@ -15,7 +17,10 @@ router.route("/get-video/:videoId").get(getVideoById);
 router
   .route("/upload-video")
   .post(verifyJwt, upload.single("video"), uploadVideo);
-// router.route("/update-video/:videoId").patch(verifyJwt, updateVideo);
+router.route("/link-video-to-crop/:id").post(verifyJwt, uploadVideoToCropVideo);
+router
+  .route("/unlink-video-from-crop/:id")
+  .delete(verifyJwt, deleteVideoFromCropVideo);
 router.route("/delete-video/:videoId").delete(verifyJwt, deleteVideo);
 
 export default router;
